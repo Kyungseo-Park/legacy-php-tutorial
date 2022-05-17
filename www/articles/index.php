6da43731b8375dd58ac284c35e787aaf30061a59
Kyungseo-Park <troublesome.dev@gmail.com>
2022-05-18 00:03:10 +0900

    feat: 글 상세 페이지 레이아웃 잡음

--- www/articles/index.php
+++ www/articles/index.php
@@ -12,8 +12,39 @@
 <body>
     <?php include "$_SERVER[DOCUMENT_ROOT]/include/header.php"; ?>
     <main>
-        
-        
+        <div class="container">
+            <div class="content-header">
+                <img class="card-img-top" src="/assets/images.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <p class="fw-bold mb-1">제목이 들어갈 자리</p>
+                    <p class="fw-normal mb-1">한줄 소개가 들어갈 자리</p>
+                    <p class="fw-normal mb-1">스택이 들어갈 자리</p>
+                    <p class="fw-lighter mb-1">날짜가 들어갈 자리</p>
+                </div>
+            </div>
+            <div class="content-body">
+
+            </div>
+            <div class="comment">
+                <div class="comment-header">
+                    <!-- 로그인한 사용자만 댓글 작성이 가능 -->
+                    <form action="" method="post">
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <button></button>
+                        <input type="button" onclick="alert('로그인 해야함'); return false;" value="로그인">
+                    </form>
+                </div>
+                <div class="comment-footer">
+                    <?php 
+                    for ($i=0; $i < 10; $i++) {  ?>
+                    <div class="">
+                        <img src="" alt="">
+                        <span>댓글내용 <?= $i ?></span>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
     </main>
     <?php include "$_SERVER[DOCUMENT_ROOT]/include/footer.php"; ?>
 </body>
