aef504f7972d332f62fe3999848b5c7e3b50b602
Kyungseo-Park <troublesome.dev@gmail.com>
2022-05-20 22:59:58 +0900

    feat(Table): 페이지네이션 추가

--- www/table.php
+++ www/table.php
@@ -11,6 +11,21 @@
 
 <body>
     <?php include "$_SERVER[DOCUMENT_ROOT]/include/header.php"; ?>
+    <?php 
+        $app_url = 'http://localhost/table.php';
+        $total = 108;
+        $per_page = 15;
+        $first_page = 1;
+        $last_page = ceil($total / $per_page);
+        $start_post = $_GET['page'] ? $_GET['page'] * $per_page - 14 : 1;
+        $end_post = $start_post + $per_page - 1;
+        if ($end_post >= $total) {
+            $end_post = $total;
+        }
+        $current_page = $_GET['page'] ? $_GET['page'] : $first_page;
+        $first_page_url = "$app_url?page=$first_page";
+        $last_page_url = "$app_url?page=$last_page";
+?>
     <main>
         <div class="container">
             <div class="row justify-content-center">
@@ -31,7 +46,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php for ($i=0; $i < 16 ; $i++) { ?>
+                            <?php for ($i = $start_post; $i <= $end_post  ; $i++) { ?>
                             <tr>
                                 <th scope="row"><?= $i ?></th>
                                 <td>
@@ -79,17 +94,37 @@
                 <div class="col-md-8 col-12">
                     <nav aria-label="Page navigation">
                         <ul class="pagination justify-content-center">
-                            <!-- 첫 페이지인 경우 disabled -->
-                            <li class="page-item disabled">
-                                <a class="page-link" tabindex="-1" aria-disabled="true">Previous</a>
-                            </li>
-                            <li class="page-item"><a class="page-link">1</a></li>
-                            <li class="page-item"><a class="page-link">2</a></li>
-                            <li class="page-item"><a class="page-link">3</a></li>
-                            <!-- 마지막 페이지인 경우 disabled -->
-                            <li class="page-item">
-                                <a class="page-link">Next</a>
-                            </li>
+                            <?php if ($first_page == $current_page) { ?>
+                                <!-- 첫 페이지인 경우 disabled -->
+                                <li class="page-item disabled">
+                                    <a class="page-link" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                            <?php } else { ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="<?= $first_page_url ?>" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                            <?php } ?>
+                            <?php for ($i = 1; $i <= $last_page ; $i++) {  ?>
+                                <?php if ($i == $current_page ) { ?>
+                                    <li class="page-item active">
+                                        <a href=<?= "$app_url?page=$i" ?> class="page-link"><?= $i ?></a>
+                                    </li>
+                                <?php } else { ?>
+                                    <li class="page-item">
+                                        <a href=<?= "$app_url?page=$i" ?> class="page-link"><?= $i ?></a>
+                                    </li>
+                                <?php } ?>
+                            <?php } ?> 
+                            <?php  if ($last_page == $current_page) { ?> 
+                                <!-- 마지막 페이지인 경우 disabled -->
+                                <li class="page-item disabled">
+                                    <a class="page-link">Next</a>
+                                </li>
+                            <?php } else { ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="<?= $last_page_url ?>" tabindex="-1" aria-disabled="true">Next</a>
+                                </li>
+                            <?php } ?>
                         </ul>
                     </nav>
                 </div>
